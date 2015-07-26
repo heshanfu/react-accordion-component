2912374c80c0bf80af634d4df86567cbb8026f31
Saul Cabrera <saulecabrera@gmail.com>
2015-07-26 16:39:24 -0500

    tested gulp tasks

---
+++ js/accordion.js
@@ -0,0 +1,11 @@
+var React =  require('React');
+
+var Accordion = React.createClass({displayName: "Accordion",
+  render: function() {
+    return(
+      React.createElement("div", null, 
+        React.createElement("div", null)
+      )
+    );
+  }
+});
---
+++ jsx/accordion.jsx
@@ -0,0 +1,11 @@
+var React =  require('React');
+
+var Accordion = React.createClass({
+  render: function() {
+    return(
+      <div>
+        <div></div>
+      </div>
+    );
+  }
+});
