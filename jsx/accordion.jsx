f0bd2997b6dcebdbbf3e54ef39d234e2ae2ba226
Saul Cabrera <saulecabrera@gmail.com>
2015-07-27 10:49:54 -0500

    added full logic, moving to styles

--- jsx/accordion.jsx
+++ jsx/accordion.jsx
@@ -1,11 +1,58 @@
 var React =  require('React');
 
-var Accordion = React.createClass({
+var AccordionElement =  React.createClass({
+  
+  propTypes: {
+    title: React.propTypes.string,
+    clickCallback: React.propTypes.func
+  },
+
+  getInitialState: function() {
+    return {collapsed: true}
+  },
+
+  _expand: function() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  },
+
   render: function() {
-    return(
-      <div>
-        <div></div>
+    var inline = {
+      display: 'inline'
+    };
+
+    return (
+      <div className='accordion-element'>
+        <div className='left-part' style={inline} onClick={this._expand}>{this.state.collapsed ? '●' : '◦'}</div>
+        <div className='right-part' style={inline} onClick={this.props.clickCallback}>
+          <h4>{this.props.title}</h4>
+        </div>
       </div>
     );
   }
+
+});
+
+/* Accordion element list */
+var Accordion = React.createClass({
+
+  propTypes: {
+    elements: React.propTypes.arrayOf(React.propTypes.object)  
+  },
+
+  render: function() {
+    var accordionElements = this.props.elements.map(function(i, e){
+      return (
+        <AccordionElement clickCallback={e.onClick} title={e.title}>
+      );
+    });
+    
+    return (
+      <div className='accordion-wrapper'>
+        {accordionElements}
+      </div>
+    );
+  }
+
 });
